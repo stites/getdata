1b23f05f295a676a1e116fcc74b6a86a86d0c06d
stites <sam@stites.io>
2014-11-22 22:48:05 -0800

    rm - helpers

--- helpers.R
+++
@@ -1,7 +0,0 @@
-dl <- function (url, short){
-  linkPartials <- strsplit(url, "\\.")
-  filetype <- as.character(tail(data.frame(linkPartials[[1]]),1)[[1]])
-  destfilename <- paste("./",short,'.', filetype, sep="")
-  download.file(url, destfile=destfilename, method="curl")
-  destfilename
-}
